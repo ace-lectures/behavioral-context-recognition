32547ffa5d7bbc686794f008b907e23647ccd723
Sébastien Mosser <sebastien.mosser@gmail.com>
2020-06-22 23:11:05 -0400

    reconnaissance de contexte v2

--- sandbox.R
+++ sandbox.R
@@ -1,11 +1,21 @@
 
 
-labels_count <- data.frame(as.list(colSums(labels, na.rm = TRUE)))
-transposed <- rownames_to_column(data.frame(t(labels_count)))
-names(transposed) <- c("variable", "n")
-transposed$variable=factor(transposed$variable,levels=transposed$variable)
-ordered <- transposed[order(transposed$n),]
-x <- ggplot(data=ordered, mapping=aes(x=variable, y=n)) + 
-  geom_bar(stat = "identity") +
-  coord_flip()
-x
+num_features <- 60 # how many features
+num_samples <- 300 # how many samples
+DATASET <- matrix(runif(num_features * num_samples),
+                  nrow = num_samples, ncol = num_features)
+
+# setting some dummy names for the features e.g. f23
+colnames(DATASET) <- paste0("f", 1:ncol(DATASET))
+
+# let's make 30% of all features to be correlated with feature "f1"
+num_feat_corr <- num_features * .3
+idx_correlated_features <- as.integer(seq(from = 1,
+                                          to = num_features,
+                                          length.out = num_feat_corr))[-1]
+for (i in idx_correlated_features) {
+  DATASET[,i] <- DATASET[,1] + runif(num_samples) # adding some noise
+}
+
+corrplot(cor(DATASET), diag = FALSE, order = "FPC",
+         tl.pos = "td", tl.cex = 0.5, method = "color", type = "upper")
